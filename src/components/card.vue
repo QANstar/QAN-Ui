0c22fc124fb417abab3f7ed08cafd46550883b98
QANstar <1435385924@qq.com>
2022-03-17 13:57:43 +0800

    卡片组件添加隐藏内容

--- src/components/card.vue
+++ src/components/card.vue
@@ -15,6 +15,28 @@
     <div class="item">
       <QANcard width="600px" round>设置宽度600px</QANcard>
     </div>
+    <div class="item">
+      <QANcard width="600px" hide round
+        >额外内容
+
+        <div slot="hide">
+          <code>
+            <div style="height:500px">1413</div>
+          </code>
+        </div>
+      </QANcard>
+    </div>
+    <div class="item">
+      <QANcard width="600px" hide shadow="hover"
+        >额外内容
+
+        <div slot="hide">
+          <code>
+            <div style="height:500px">6666</div>
+          </code>
+        </div>
+      </QANcard>
+    </div>
   </div>
 </template>
 
--- src/plugin/components/card/src/card.vue
+++ src/plugin/components/card/src/card.vue
@@ -4,15 +4,54 @@
     class="qan-card"
     :class="['qan-card-shadow-' + shadow, { 'qan-card-round': round }]"
   >
-    <slot></slot>
+    <div class="qan-card-content">
+      <slot></slot>
+    </div>
+    <div v-if="hide">
+      <div
+        ref="hide"
+        :style="{ height: hideHeight }"
+        class="qan-card-hide-content"
+      >
+        <slot name="hide">{{ hide }}</slot>
+      </div>
+      <div @click="hideClick" class="qan-card-hide">
+        {{ hideText }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "QANcard",
+  data() {
+    return {
+      hideText: "显示内容",
+      hideHeight: "0px",
+      isShow: false,
+    };
+  },
+  methods: {
+    hideClick() {
+      this.isShow = !this.isShow;
+      if (this.isShow) {
+        this.hideHeight = this.hideShowHeight;
+        this.hideText = "隐藏内容";
+      } else {
+        this.hideHeight = "0px";
+        this.hideText = "显示内容";
+      }
+    },
+  },
+  computed: {
+    hideShowHeight() {
+      return this.$refs.hide.children[0].clientHeight + "px";
+    },
+  },
   props: {
     round: Boolean,
+    hide: Boolean,
     shadow: {
       type: String,
       default: "",
