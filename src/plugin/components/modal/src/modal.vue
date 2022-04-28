5e4cd92bca72fd4628f7c7061409c74cfe805d98
QANstar <1435385924@qq.com>
2022-04-28 17:55:43 +0800

    优化模态框

--- src/plugin/components/modal/src/modal.vue
+++ src/plugin/components/modal/src/modal.vue
@@ -8,37 +8,7 @@
   </transition>
 </template>
 
-<style scoped>
-.modal-enter-active {
-  animation: modal-in 0.3s;
-}
 
-.modal-leave-active {
-  animation: modal-out 0.3s;
-}
-
-@keyframes modal-in {
-  0% {
-    transform: translateY(-30px);
-    opacity: 0;
-  }
-  100% {
-    transform: translateY(0px);
-    opacity: 1;
-  }
-}
-
-@keyframes modal-out {
-  0% {
-    transform: translateY(0px);
-    opacity: 1;
-  }
-  100% {
-    transform: translateY(-30px);
-    opacity: 0;
-  }
-}
-</style>
 <script>
 export default {
   name: "QANmodal",
