6731ebd979a184a925b7eb328a8e1c68a7b0bcf0
QANstar <1435385924@qq.com>
2022-04-28 17:44:39 +0800

    完善模态框动画

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -3,12 +3,16 @@
     <h3>Modal 模态框</h3>
     <div class="intr-font">保留当前页面，弹出额外内容</div>
     <h4>基础使用</h4>
-    <div class="intr-font">QANmodal标签来创建模态框</div>
+    <div class="intr-font">
+      QANmodal标签来创建模态框,设置visible属性来显示或隐藏模态框,使用width来设定宽度
+    </div>
     <QANcard class="show-card" width="80%" round hide shadow="hover">
       <QANbutton @click="visible = true" type="pink">点击打开模态框</QANbutton>
-      <QANmodal :visible="visible">模态框</QANmodal>
+      <QANmodal width="50%" :visible.sync="visible">
+        <div class="modal-base">这是模态框内容</div>
+      </QANmodal>
       <div slot="hide">
-        <div class="code-show">{{ cardUse }}</div>
+        <div class="code-show">{{ modalBase }}</div>
       </div>
     </QANcard>
   </div>
@@ -18,6 +22,14 @@
 .card-item {
   margin: 10px;
 }
+.modal-base {
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font: 18px;
+  font-weight: bold;
+}
 </style>
 
 <script>
@@ -25,25 +37,10 @@ export default {
   data() {
     return {
       visible: false,
-      cardUse: "<QANcard>卡片</QANcard>",
-      cardShadow:
-        '<div class="card-item">\n' +
-        "   <QANcard>卡片</QANcard>\n" +
-        "</div>\n" +
-        '<div class="card-item">\n' +
-        '   <QANcard shadow="always">阴影卡片</QANcard>\n' +
-        "</div>\n" +
-        '<div class="card-item">\n' +
-        '   <QANcard shadow="hover">覆盖阴影卡片</QANcard>\n' +
-        "</div>\n",
-      cardRound: "<QANcard round>圆角卡片</QANcard>",
-      cardWidth: '<QANcard width="600px" round>设置宽度600px</QANcard>',
-      cardHide:
-        '<QANcard width="600px" hide shadow="hover">额外内容\n' +
-        '   <div slot="hide">\n' +
-        '       <div style="height: 200px">这里是隐藏内容</div>\n' +
-        "   </div>\n" +
-        "</QANcard>\n",
+      modalBase:
+        '<QANmodal width="50%" :visible.sync="visible">\n' +
+        '   <div class="modal-base">这是模态框内容</div>\n' +
+        "</QANmodal>\n",
     };
   },
 };
--- src/plugin/components/modal/src/modal.vue
+++ src/plugin/components/modal/src/modal.vue
@@ -1,13 +1,44 @@
 <template>
-  <transition>
+  <transition name="modal">
     <div v-show="visible" @click.self="wrapperClick" class="qan-modal-wrapper">
-      <div class="qan-modal-content">
+      <div :style="{ width: width }" class="qan-modal-content">
         <slot></slot>
       </div>
     </div>
   </transition>
 </template>
 
+<style scoped>
+.modal-enter-active {
+  animation: modal-in 0.3s;
+}
+
+.modal-leave-active {
+  animation: modal-out 0.3s;
+}
+
+@keyframes modal-in {
+  0% {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+}
+
+@keyframes modal-out {
+  0% {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
+</style>
 <script>
 export default {
   name: "QANmodal",
@@ -16,13 +47,17 @@ export default {
       type: Boolean,
       default: false,
     },
+    width: {
+      type: String,
+      default: "400px",
+    },
   },
   data() {
     return {};
   },
   methods: {
     wrapperClick() {
-      this.visible = false;
+      this.$emit("update:visible", false);
     },
   },
 };
